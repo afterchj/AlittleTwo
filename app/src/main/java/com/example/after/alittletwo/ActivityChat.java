2818e95507cf038cfd4eeaa640b4094fbde1c321
hongjian.chen <766256898@qq.com>
2018-07-26 15:17:51 +0800

    正式版本1.0.2上线

--- app/src/main/java/com/example/after/alittletwo/ActivityChat.java
+++ app/src/main/java/com/example/after/alittletwo/ActivityChat.java
@@ -45,24 +45,18 @@ public class ActivityChat extends AppCompatActivity {
     private String userid, taskid;
     private int allcount;
     private Bitmap bitmap;
-//    private SwipeRefreshLayout uploadmore;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                bitmap = BitmapFileSetting.getByUrl(Constant.HEAD.getBaseUrl());
-//            }
-//        }).start();
+
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activitychat);
         bitmap = BitmapFactory.decodeFile(filePath);
         initView();
-//                userid = "d8428fb4e425425db75c1c7d1e47bbc0";//苏小美;
-        userid = "d35558c810fd4b9ea3b7482af39ad51d";//王大锤
+        userid = "d8428fb4e425425db75c1c7d1e47bbc0";//苏小美;
+//        userid = "d35558c810fd4b9ea3b7482af39ad51d";//王大锤
         taskid = "cd48e5b34c29448c98d20fa6869c3647";
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
@@ -71,8 +65,8 @@ public class ActivityChat extends AppCompatActivity {
         String yourId = intent.getStringExtra("yourid");
         String topicId = intent.getStringExtra("taskid");
         if (friendId != null && !friendId.isEmpty()) {
-//            userid = yourId;
-            userid = friendId;
+            userid = yourId;
+//            userid = friendId;
         }
         if (topicId != null && !topicId.isEmpty()) {
             taskid = topicId;
